a6ddcd7022f87f4887612126e069cb97d3c5072f
Catherine0120 <shiying-21@mails.tsinghua.edu.cn>
2022-08-31 20:20:57 +0800

    updates detail_news_page function

--- MyNewsApp/app/src/main/java/com/java/zhangshiying/DetailNewsActivity.java
+++ MyNewsApp/app/src/main/java/com/java/zhangshiying/DetailNewsActivity.java
@@ -8,8 +8,11 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -28,6 +31,8 @@ import java.net.URL;
 public class DetailNewsActivity extends AppCompatActivity {
     News news;
     ImageView topImageView;
+    HorizontalScrollView topImagesScrollView;
+    LinearLayout myLinearLayout;
 
     private class DetailHandler extends Handler {
         private final WeakReference<DetailNewsActivity> myAcitivity;
@@ -39,11 +44,22 @@ public class DetailNewsActivity extends AppCompatActivity {
         public void handleMessage(@NonNull Message msg) {
             switch(msg.what) {
                 case 1:
-                    topImageView.setVisibility(View.VISIBLE);
-                    try {
-                        topImageView.setImageBitmap((Bitmap)msg.obj);
-                    } catch (Exception e) { e.printStackTrace();}
-                    break;
+                    if (!(msg.getData().getBoolean("multiImages"))) {
+                        topImageView.setVisibility(View.VISIBLE);
+                        try {
+                            topImageView.setImageBitmap((Bitmap)((Object[]) msg.obj)[0]);
+                        } catch (Exception e) { e.printStackTrace();}
+                        break;
+                    }
+                    else {
+                        try {
+                            View view = (View)(((Object[]) msg.obj)[1]);
+                            ImageView img = (ImageView) (((Object[]) msg.obj)[2]);
+                            img.setImageBitmap((Bitmap)((Object[]) msg.obj)[0]);
+                            myLinearLayout.addView(view);
+                        } catch (Exception e) { e.printStackTrace();}
+                        break;
+                    }
                 default:
                     break;
             }
@@ -71,14 +87,25 @@ public class DetailNewsActivity extends AppCompatActivity {
         contentDetail.setText(news.content);
 
         if (news.imageExist) {
-            topImageView = (ImageView) findViewById(R.id.image_detail);
-            topImageView.setVisibility(View.VISIBLE);
-            System.out.println(topImageView.getHeight());
-            getBitmapFromURL(news.imageUrls.get(0));
+            if (news.imageCount == 1) {
+                topImageView = (ImageView) findViewById(R.id.image_detail);
+                topImageView.setVisibility(View.VISIBLE);
+                getBitmapFromURL(news.imageUrls.get(0), false, null, null);
+            }
+            else {
+                topImagesScrollView = (HorizontalScrollView) findViewById(R.id.horizontal_scroll_view_detail);
+                topImagesScrollView.setVisibility(View.VISIBLE);
+                myLinearLayout = (LinearLayout) findViewById(R.id.image_horizontal_layout);
+                for (int i = 0; i < news.imageCount; ++i) {
+                    View view = LayoutInflater.from(this).inflate(R.layout.single_image_layout, myLinearLayout, false);
+                    ImageView img = view.findViewById(R.id.single_image);
+                    getBitmapFromURL(news.imageUrls.get(i), true, view, img);
+                }
+            }
         }
     }
 
-    private void getBitmapFromURL(String src) {
+    private void getBitmapFromURL(String src, boolean multiImages, View view, ImageView img) {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -95,8 +122,12 @@ public class DetailNewsActivity extends AppCompatActivity {
 //                    Log.e("Bitmap","returned");
 
                     Message msg = new Message();
-
-                    msg.obj = myBitmap;
+                    Bundle bundle = new Bundle();
+                    bundle.putBoolean("multiImages", multiImages);
+                    msg.setData(bundle);
+                    msg.obj = new Object[] {myBitmap, view, img};
+                    System.out.println((Bitmap)((Object[]) msg.obj)[0]);
+                    System.out.println(myBitmap + " " + view + " " + img);
                     msg.what = 1;
                     myHandler.sendMessage(msg);
 
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 `[Log 2022/8/31 11:04]`：实现下拉加载。
 
-`[Log 2022/8/31 17:46]`：实现`DetailNewsActivity`
+`[Log 2022/8/31 20:20]`：实现`DetailNewsActivity`
 
 
 
@@ -18,4 +18,4 @@
 1. `BottomNavigation + ViewPager2 + Fragment`（包括`scroll`的时候`Navigation`收缩等）
 2. 图片加载问题：`java.net.MalFormedURLException: no protocol`，`D/skia:---Failed to create image decoder with message 'unimplemented'`
 3. 视频播放
-3. `detailed page`中图片过大问题
+3. ~~`detailed page`中图片过大问题~~
